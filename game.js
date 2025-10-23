0976447ab8e18b7918e2f49971cf811793a1c8d8
Digitalknuckles <digitalknuckles@gmail.com>
2025-10-23 13:53:28 -0400

    Update game.js

--- game.js
+++ game.js
@@ -245,7 +245,7 @@ const rug2 = this.physics.add.sprite(225, 200, 'rug2')
   tenk.body.setSize(36, 36).setOffset(0, 0);
   this.physics.add.collider(player, tenk);
 
-    const punk1 = this.physics.add.sprite(300, 289, 'wall2')
+    const punk1 = this.physics.add.sprite(300, 285, 'wall2')
     .setImmovable(true)
     .setOrigin(0, 0)
     .setDisplaySize(60, 64);
@@ -428,6 +428,7 @@ function collectItem(player, item) {
     });
   }
 }
+
 
 
 
