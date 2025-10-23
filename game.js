531a348223d13c73045d02b25664b46c7886c94d
Digitalknuckles <digitalknuckles@gmail.com>
2025-10-23 13:16:37 -0400

    Update game.js

--- game.js
+++ game.js
@@ -125,7 +125,7 @@ const rug2 = this.physics.add.sprite(225, 200, 'rug2')
     punk = this.physics.add.sprite(150, 50, 'Punk1')
     .setImmovable(true)
     .setOrigin(0, 0)
-    .setDisplaySize(50, 72);
+    .setDisplaySize(40, 100);
   punk.body.setSize(50, 50);
   punk.body.setOffset(0, 0);
 
@@ -426,6 +426,7 @@ function collectItem(player, item) {
     });
   }
 }
+
 
 
 
